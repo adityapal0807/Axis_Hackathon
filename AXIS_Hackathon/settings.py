b1278c27aff4318a62b2afa5ed2a949e119a8b52
Aditya Pal Singh <64854366+adityapal0807@users.noreply.github.com>
2023-08-17 00:33:18 +0530

    Update settings.py

--- AXIS_Hackathon/settings.py
+++ AXIS_Hackathon/settings.py
@@ -140,5 +140,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'palsaab0807@gmail.com'
-EMAIL_HOST_PASSWORD = 'fhaezqkxcpniozay'
+EMAIL_HOST_PASSWORD = ''
 
